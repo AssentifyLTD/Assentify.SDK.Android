bb1992a986bfaabd9d003905016ef4f1b25dbf32
Tariq <alskrantariq@gmail.com>
2026-01-05 18:13:23 +0300

    14

--- app/src/main/java/com/assentify/sdk/Flow/FlowController/FlowController.kt
+++ app/src/main/java/com/assentify/sdk/Flow/FlowController/FlowController.kt
@@ -34,7 +34,7 @@ object FlowController {
         val currentStep = getCurrentStep();
         if (currentStep == null) {
             val intent = Intent(context, SubmitStepActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             context.startActivity(intent)
         } else {
             when (currentStep.stepDefinition!!.stepDefinition) {
