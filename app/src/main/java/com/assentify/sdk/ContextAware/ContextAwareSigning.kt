10d240923e869dadaa044e76e4cc03094d1c3d6e
AssentifyLTD <info.assentify@gmail.com>
2026-05-14 15:45:53 +0300

    Update ContextAwareSigning.kt

--- app/src/main/java/com/assentify/sdk/ContextAware/ContextAwareSigning.kt
+++ app/src/main/java/com/assentify/sdk/ContextAware/ContextAwareSigning.kt
@@ -134,7 +134,7 @@ class ContextAwareSigning(
             signature = signature,
         )
         val remoteService = remoteSigningService
-        val call = remoteService.signature(signatureRequestModel)
+        val call = remoteService.signature(signatureRequestModel,configModel.tenantIdentifier)
         call.enqueue(object : Callback<SignatureResponseModel> {
             override fun onResponse(
                 call: Call<SignatureResponseModel>,
