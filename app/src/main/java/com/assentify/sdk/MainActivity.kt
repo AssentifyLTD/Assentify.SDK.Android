78dd26083c661ead593b5417d49020162b0521c4
Tariq <alskrantariq@gmail.com>
2026-08-19 12:50:39 +0300

    34

--- app/src/main/java/com/assentify/sdk/MainActivity.kt
+++ app/src/main/java/com/assentify/sdk/MainActivity.kt
@@ -190,7 +190,7 @@ class MainActivity : AppCompatActivity(), AssentifySdkCallback, FlowCallBack {
                     activeLivenessCheckCount = 3,
                     faceLivenessRetryCount = 2,
                     minRam = 1,
-                    flowUiLanguage = UiLanguage.English
+                    flowUiLanguage = UiLanguage.Arabic
                 );
                 assentifySdk = AssentifySdk(
                     apiKey = config.apiKey,
@@ -283,7 +283,7 @@ class MainActivity : AppCompatActivity(), AssentifySdkCallback, FlowCallBack {
 
 
 
-          //  assentifySdk.clearFlow(this@MainActivity,)
+           assentifySdk.clearFlow(this@MainActivity,)
             assentifySdk.startFlow(
                 this@MainActivity,
                 flowCallback = this,
